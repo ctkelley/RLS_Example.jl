29589c58a164e5b727b793354c9e81a6ac11bdb7
Tim Kelley <ctk@ncsu.edu>
2024-06-11 11:23:06 -0400

    More prep work

--- src/RLS_Example.jl
+++ src/RLS_Example.jl
@@ -16,9 +16,8 @@ include("D1.jl")
 include("G2.jl")
 include("Green_Utils.jl")
 include("Generic_Utils.jl")
-include("Simple.jl")
-include("Chen_Example.jl")
 include("HistPrint.jl")
+include("fprintTeX.jl")
 
 export HistPrint
 export PlotNew
